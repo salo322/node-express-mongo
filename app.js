627a8b240ce5de14b2cb3aaaaf8a98a3d0d7a25c
salo322 <48085066+salo322@users.noreply.github.com>
2022-05-12 23:15:03 +0400

    update

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const blogRoutes = require("./routes/blogRoutes");
 const app = express();
-
+app.use(function (req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*"); // <<<<<<<<<<<<
+   res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+   );
+   next();
+});
 app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
